8b8ad50489a395c87ab8c40b990c7b54be860d2d
Eduardo Santos <edumaxsantos@gmail.com>
2017-03-08 19:06:56 -0300

    Erros na classe

--- database/migrations/2017_02_13_181254_create_produtos_table.php
+++ database/migrations/2017_02_13_181254_create_produtos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreteProdutosTable extends Migration
+class CreateProdutosTable extends Migration
 {
     /**
      * Run the migrations.

dd24efc582443b28d7bcf583e7d50d79f5f61268
Gabriel Santos <gabmaxsan@gmail.com>
2017-05-03 22:51:08 -0300

    heroku

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_44f25896ef77dba'),
+            'username' => env('DB_USERNAME', 'b3475e50b9492b'),
+            'password' => env('DB_PASSWORD', '27a74b91'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
